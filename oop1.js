e84d705aa900befa1e4e83ffa0c66a02e4f85ed1
BKRavindu <bkravindu2000@gmai.com>
2023-12-01 20:16:20 +0530

    new code add

--- oop1.js
+++ oop1.js
@@ -2,10 +2,22 @@ var shoes1 = {
     shoes1price:100,
     tax:1.2,
     total:function(){
-        return this.shoes1*this.tax
+        return this.shoes1price*this.tax
     }
 
 }
+
+var shoes2 = {
+    shoes2price:50,
+    tax:1.2,
+    total:function(){
+        return shoes2.shoes2price*shoes2.tax
+    }
+
+}
+
+var y = shoes2.total()
+console.log(y)
 
 var x = shoes1.total()
 console.log(x)
